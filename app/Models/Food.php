9717c310b978d52760c20557e7b739e33546eb76
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2023-01-30 19:49:17 +0700

    model food dan transactions

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Food extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'ingredients', 'rate', 'price','types',
+        'picturePath'
+    ];
+
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAttribute($value){
+        return Carbon::parse($value)->timestamp;
+    }
+
+    //picturePath bisa dipanggil
+    public function toArray(){
+        $toArray = parent::toArray();
+        $toArray['picturePath'] = $this->picturePath;
+        return $toArray;
+    }
+
+    public function getPicturePathAttribute(){
+        return url(''). Storage::url($this->attributes['picturePath']);
+    }
+
+
 }
+  
