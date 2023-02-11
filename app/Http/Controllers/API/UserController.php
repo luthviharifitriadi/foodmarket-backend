a582aa98da27919bd63e79acc67a3ad83ce427cb
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2023-02-11 15:06:49 +0700

    Add Api Register

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\API;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserController extends Controller
 {
+
     public function login(Request $request){
         try {
             //validasi input
@@ -49,5 +52,41 @@ class UserController extends Controller
         }
     }
 
+    public function register(Request $request){
+        try {
+            $request-validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' =>  $this->passwordRules(),
+
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'address' => $request->address,
+                'houseNumber' => $request->houseNumber,
+                'phoneNumber' => $request->phoneNumber,
+                'city' => $request->city,
+                'password' => Hash::make($request->password)
+            ]);
+            $user = User::where('email', $request->email)->first();
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;  
+            
+            return ResponseFormater::success([
+                'access_token' => $tokenResult,
+                'toke_type' => 'Bearer',
+                'user' =>$user
+            ]);
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    
     
 }
