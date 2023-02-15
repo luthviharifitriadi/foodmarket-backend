b0d3a9e0dbb31b6701b00607cb77c722c091e48d
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2023-02-15 22:32:23 +0700

    update photo

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -9,6 +10,8 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\validator;
+
 
 
 class UserController extends Controller
@@ -105,4 +108,30 @@ class UserController extends Controller
 
         return ResponseFormatter::success($user, 'Profile update');
     }
+
+    public function updatePhoto(Request $request){
+        $validator = Validator::make($request->all(),[
+            'file' => 'required|image|max:2048'
+        ]);
+
+        if($validator->fails())
+        {
+            return ResponseFormatter::error([
+                'error' => $validator->errors()
+            ], 'update photo fails', 401);
+        }
+
+        if($request->file('file')){
+            $file = $request->file->store('assets/user', 'public');
+
+            //simpan foto ke database (urlsaja)
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file], 'file successfully updload');
+
+        }
+    }
+    
 }
