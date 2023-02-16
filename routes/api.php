96ef69aa4a88b47f4ad546ee7e2b0b93e3904a44
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2023-02-16 18:32:31 +0700

    membuat api food

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controller\API\UserController;
+use App\Http\Controller\API\FoodController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,9 +21,9 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('user', [UserController::class, 'updateProfile']);
     Route::post('user/photo', [UserController::class, 'updatePhoto']);
     Route::post('logout', [UserController::class, 'logout']);
-    
 
-});
+}); 
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
+Route::get('food', [FoodController::class, 'all']);
