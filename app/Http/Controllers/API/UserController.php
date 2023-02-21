a5c895901af83c24a8dd7e9e11c4318a0717702f
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2023-02-21 19:11:50 +0700

    testing bug  api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,58 +10,72 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\validator;
-
-
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    use PasswordValidationRules;
 
-    public function login(Request $request){
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success($request->user(),'Data profile user berhasil diambil');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function login(Request $request)
+    {
         try {
-            //validasi input
             $request->validate([
-              'email'       => 'email|required',
-              'password'    => 'required'  
+                'email' => 'email|required',
+                'password' => 'required'
             ]);
 
-            //mengecek credentials (login)
             $credentials = request(['email', 'password']);
-            if(!Auth::attempt($credentials)) {
-                return RessponseFormatter::error([
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
                     'message' => 'Unauthorized'
-                ], 'Authentication Failed', 500);
+                ],'Authentication Failed', 500);
             }
 
-            //Jika hash tidak sesuai maka beri error
-            $user = User::where('email', $request-email)->first();
-            if(!Hash::check($request->password, $user->password, [])){
+            $user = User::where('email', $request->email)->first();
+            if ( ! Hash::check($request->password, $user->password, [])) {
                 throw new \Exception('Invalid Credentials');
             }
-            // jika berhasil maka loginkan
-            $tokeResult = $user->createToken('authToken')->plainTextToken;
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
             return ResponseFormatter::success([
-                'access_token'   => $tokenResult,
-                'token_type'     => 'Bearer',
-                'user'           => $user
-            ], 'Authenticated');  
-        } 
-        // benar benar gagal
-        catch(Exception $error){
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ],'Authenticated');
+        } catch (Exception $error) {
             return ResponseFormatter::error([
-                'message'   => 'Something went wrong',
-                'error'     => $error
-            ], 'Authentication Failed', 500);
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ],'Authentication Failed', 500);
         }
     }
 
-    public function register(Request $request){
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function register(Request $request)
+    {
         try {
-            $request-validate([
+            $request->validate([
                 'name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-                'password' =>  $this->passwordRules(),
-
+                //'password' => $this->passwordRules()
             ]);
 
             User::create([
@@ -71,67 +85,63 @@ class UserController extends Controller
                 'houseNumber' => $request->houseNumber,
                 'phoneNumber' => $request->phoneNumber,
                 'city' => $request->city,
-                'password' => Hash::make($request->password)
+                'password' => Hash::make($request->password),
             ]);
+
             $user = User::where('email', $request->email)->first();
 
-            $tokenResult = $user->createToken('authToken')->plainTextToken;  
-            
-            return ResponseFormater::success([
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
                 'access_token' => $tokenResult,
-                'toke_type' => 'Bearer',
-                'user' =>$user
-            ]);
+                'token_type' => 'Bearer',
+                'user' => $user
+            ],'User Registered');
         } catch (Exception $error) {
             return ResponseFormatter::error([
                 'message' => 'Something went wrong',
-                'error' => $error
-            ], 'Authentication Failed', 500);
+                'error' => $error,
+            ],'Authentication Failed', 500);
         }
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         $token = $request->user()->currentAccessToken()->delete();
 
-        return ResponseFormatter::success($token, 'Token Revoked');
-    } 
-
-    public function fetch(Request $request){
-        return ResponseFormatter::success($request->user(), 'Data Profile user berhasil diambil');
+        return ResponseFormatter::success($token,'Token Revoked');
     }
 
-    public function updateProfile(Request $request){
+    public function updateProfile(Request $request)
+    {
         $data = $request->all();
 
         $user = Auth::user();
         $user->update($data);
 
-        return ResponseFormatter::success($user, 'Profile update');
+        return ResponseFormatter::success($user,'Profile Updated');
     }
 
-    public function updatePhoto(Request $request){
-        $validator = Validator::make($request->all(),[
-            'file' => 'required|image|max:2048'
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048',
         ]);
 
-        if($validator->fails())
-        {
-            return ResponseFormatter::error([
-                'error' => $validator->errors()
-            ], 'update photo fails', 401);
+        if ($validator->fails()) {
+            return ResponseFormatter::error(['error'=>$validator->errors()], 'Update Photo Fails', 401);
         }
 
-        if($request->file('file')){
+        if ($request->file('file')) {
+
             $file = $request->file->store('assets/user', 'public');
 
-            //simpan foto ke database (urlsaja)
+            //store your file into database
             $user = Auth::user();
             $user->profile_photo_path = $file;
             $user->update();
 
-            return ResponseFormatter::success([$file], 'file successfully updload');
-
+            return ResponseFormatter::success([$file],'File successfully uploaded');
         }
     }
-    
 }
